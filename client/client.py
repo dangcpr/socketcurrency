55d81ec46ffab7f5f8400e6bbf8f73cce608293c
dangcpr <75834491+dangcpr@users.noreply.github.com>
2021-12-02 00:31:39 +0700

    socket

--- client/client.py
+++ client/client.py
@@ -4,18 +4,41 @@ import schedule
 import time
 import socket
 import tkinter as tk
+import sys
+import os
+
+host = '127.0.0.1'
+port = 65432
+
+def createSocket():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('Da tao socket')
+    except socket.error as err:
+        print('Loi tao socket', err)
+    
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            msg = 'Hello Workd'
+            s.sendall(msg.encode('utf8'))
+            print('Da xong')
+            data = s.recv(1024)
+            print('Received ', data.decode('utf8'))
+    except socket.error as err:
+        print("Loi ket noi: ", err)
+        sys.exit(1)
+
+def getData(file):
+    with open(file, 'r') as f:
+        data = json.load(f)
+    return data
+
+#def Interface_for_Client():
 
 
 
-def data():
-    url = "https://currency-converter5.p.rapidapi.com/currency/convert"
 
-    querystring = {"format":"json","from":"AUD","to":"CAD","amount":"1"}
-
-    headers = {
-        'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
-        'x-rapidapi-key': "eeedbfa64emshbdd697d5f8b99b5p13bbcdjsn9ce5e073eaa1"
-    }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     json_object = json.loads(response.text)
@@ -23,14 +46,17 @@ def data():
     with open('data.json', 'w' , encoding='utf-8') as f:
         json.dump(json_object, f, ensure_ascii=False, indent=4)
 
-    
+
+def Example():
+    print("Hello")
+
+
+
+
+
 
     
 if __name__=="__main__":
     #data()
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname_ex(hostname)
-    print(hostname)
-    print(local_ip)
-    #createInterface()
-
+    createSocket()
+    os.system("pause")
