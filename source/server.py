d8d6bb670dcedf8e635dc398899339bbde64440b
dang252 <dang_252@yahoo.com.vn>
2021-12-15 04:15:42 +0700

    Thêm GUI ở client và đổi 1 chút ở Signup

--- source/server.py
+++ source/server.py
@@ -6,27 +6,36 @@ import socket
 import tkinter as tk
 import sys
 import os
+import threading
+import fnmatch
 
-
+connectAddress = []
 # host = '127.0.0.1'
 # port = 65432
+
 def CheckIfExit(Username, Password):
-    User = Username + '_' + Password
+    #User = Username + '_' + Password
     with open ('Account.json', 'r') as f:
         Acc_List = json.load(f)
-        if User in Acc_List['Account']:
-            return '1'
+        if (Username in Acc_List['Account']):
+            index = Acc_List['Account'].index(Username)
+            if (Acc_List['Pass'][index] == Password):
+                return '1'
+            else:
+                return '0'
         else: return '0'
 
 def SaveAccount(Username, Password):
-    User = Username + '_' + Password
+    #User = Username + '_' + Password
     with open('Account.json', 'r+') as f:
         Acc_list = json.load(f)
-        Acc_list['Account'].append(User)
+        Acc_list['Account'].append(Username)
+        Acc_list['Pass'].append(Password)
         f.seek(0)
         json.dump(Acc_list, f, indent = 4)
 
 def SignUp_server(s):
+    check = None
     checkAcc = None
     while True:
         Username = s.recv(1024).decode("utf8")
@@ -37,6 +46,10 @@ def SignUp_server(s):
         else:
             s.sendall('0'.encode('utf8'))
             SaveAccount(Username, Password)
+            return
+        check = s.recv(1).decode('utf8')
+        if check == '0':
+            Login_server(s)
             return
 
 
@@ -53,20 +66,23 @@ def Login_server(s):
             s.sendall('1'.encode('utf8'))
             return
         check = s.recv(1).decode('utf8')
-        if check == '2':
+        if check == '0':
             SignUp_server(s)
             return
 
 
-def runServer(s):
+def runServer(conn, addr):
     try:
-        conn, addr = s.accept()
         with conn:
             print('Duoc ket noi boi', addr)
+            connectAddress.append(conn)
+            print(connectAddress)
             DNDK = conn.recv(1).decode('utf8')
             if DNDK == '1':
+                print(DNDK)
                 Login_server(conn)
             else:
+                print(DNDK)
                 SignUp_server(conn)
             str_data = None
             while str_data != 'x':
@@ -77,9 +93,28 @@ def runServer(s):
                 print(str_data)
                 str_send = 'Mon loz'
                 conn.sendall(str_send.encode('utf8'))
+            print(addr, ' da ngat ket noi')
+            disAddr = str(addr)
+            index = connectAddress.index(conn)
+            connectAddress.pop(index)
+            dis = ''.join(['Client ', disAddr, ' da thoat'])
+            for i in connectAddress:
+                i.send(dis.encode('utf8'))
+
+
     except socket.error as err:
         print("Lỗi kết nối: ", err)
         sys.exit(1)
+
+def threadClient(s):
+    while True:
+        try:
+            conn, addr = s.accept()
+            thrr = threading.Thread(target = runServer, args=(conn, addr))
+            thrr.daemon = True
+            thrr.start()
+        except:
+            print('Error')
 
 
 def data():
@@ -107,6 +142,14 @@ def updateLocalIPEvery30mins():
         time.sleep(0)
 
 
+def exportCurrency():
+    with open('currency.json',encoding="utf-16-le") as f:
+        json_data = json.load(f)
+        print(json.dumps(json_data, indent = 3))
+
+
+
+
 # def exportData():
 
 
@@ -120,8 +163,9 @@ if __name__ == "__main__":
         print('Da tao socket')
     except socket.error as err:
         print('Loi tao socket', err)
-
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((address, port))
     s.listen(1)
-
-    runServer(s)
+    #print('Xin chào các bạn'.encode('utf-16'))
+    #exportCurrency()
+    threadClient(s)
